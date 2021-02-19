3262d9dcd179e248bd400afb96784ef1ee9a2a20
Lakshman <lakshman.miani@mail.com>
2021-02-19 08:33:10 +0530

    Added if-else-if and for loop programs

--- src/com/careerit/cj/basics/conditional/BiggestOfThreeNumbers.java
+++ src/com/careerit/cj/basics/conditional/BiggestOfThreeNumbers.java
@@ -1,12 +1,31 @@
 package com.careerit.cj.basics.conditional;
 
+import java.util.Scanner;
+
 public class BiggestOfThreeNumbers {
 
 	public static void main(String[] args) {
-		int a = 10;
-		int b = 20;
-		int c = 30;
-		int big = a > b && a > c ? a : b > c ? b : c;
-		System.out.println("Biggest is :"+big);
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the num1: ");
+		int num_1 = sc.nextInt();
+		System.out.println("Enter the num2: ");
+		int num_2 = sc.nextInt();
+		System.out.println("Enter the num3: ");
+		int num_3 = sc.nextInt();
+
+		int big = (num_1 > num_2 && num_1 > num_3) ? num_1 : (num_2 > num_3) ? num_2 : num_3;
+				
+//		if (num_1 > num_2 && num_1 > num_3) {
+//			big = num_1;
+//		} else if (num_2 > num_3) {
+//			big = num_2;
+//		} else {
+//			big = num_3;
+//		}
+		System.out.printf("Biggest of (%d, %d, %d) =  %d", num_1, num_2, num_3, big);
+
+		sc.close();
+
 	}
 }
