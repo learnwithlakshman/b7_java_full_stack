f2f4a4660771ba30a7032c86cfd0c53882b5f08b
Lakshman <lakshman.miani@mail.com>
2021-02-19 07:22:25 +0530

    Added logic: biggest of three numbers

--- src/com/careerit/cj/basics/conditional/BiggestOfThreeNumbers.java
+++ src/com/careerit/cj/basics/conditional/BiggestOfThreeNumbers.java
@@ -2,4 +2,11 @@ package com.careerit.cj.basics.conditional;
 
 public class BiggestOfThreeNumbers {
 
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		int big = a > b && a > c ? a : b > c ? b : c;
+		System.out.println("Biggest is :"+big);
+	}
 }
